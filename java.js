8d1be05bb2f6ad0a91d687374e36e890e7b0d142
Service-tecnic <151688953+Service-tecnic@users.noreply.github.com>
2023-11-30 00:48:43 +0100

    initial commit

---
+++ java.js
@@ -0,0 +1,18 @@
+function mostrar() {
+    document.getElementById('img').style.display = 'block';
+}
+
+function ocultar() {
+    document.getElementById('img').style.display = 'none';
+}
+
+
+
+
+function ver() {
+    document.getElementById('imagi').style.display = 'block';
+}
+
+function nover() {
+    document.getElementById('imagi').style.display = 'none';
+}
